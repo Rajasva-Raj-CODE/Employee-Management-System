1f01bde06e3e16d68c6b1109f014b0fabe8eee27
Rajasva-Raj-CODE <rajasvarajshrivastava@gmail.com>
2024-10-19 18:02:16 +0530

    employee dasdboard done task list  done

--- src/Components/TaskList/TaskList.jsx
+++ src/Components/TaskList/TaskList.jsx
@@ -1,19 +1,88 @@
-import React from 'react'
+import React from "react";
 
 const TaskList = () => {
   return (
-    <div id='tasklist' className='h-[55%] w-full py-5 mt-10 flex items-center justify-start gap-5 flex-nowrap overflow-x-auto'>
-        <div className='h-full w-[300px] bg-yellow-400 rounded-xl flex-shrink-0'></div>
-        <div className='h-full w-[300px] bg-yellow-400 rounded-xl flex-shrink-0'></div>
-        <div className='h-full w-[300px] bg-yellow-400 rounded-xl flex-shrink-0'></div>
-        <div className='h-full w-[300px] bg-yellow-400 rounded-xl flex-shrink-0'></div>
-        <div className='h-full w-[300px] bg-yellow-400 rounded-xl flex-shrink-0'></div>
-        <div className='h-full w-[300px] bg-yellow-400 rounded-xl flex-shrink-0'></div>
-        <div className='h-full w-[300px] bg-yellow-400 rounded-xl flex-shrink-0'></div>
-        <div className='h-full w-[300px] bg-yellow-400 rounded-xl flex-shrink-0'></div>
+    <div
+      id="tasklist"
+      className="h-[55%] w-full py-5 mt-10 flex items-center justify-start gap-5 flex-nowrap overflow-x-auto"
+    >
+      <div className="h-full w-[300px] bg-yellow-400 rounded-xl flex-shrink-0 p-5">
+        <div className=" flex justify-between items-center">
+          <h3 className="bg-red-600 text-sm px-3 py-1 rounded">Hight</h3>
+          <h4 className="text-sm">20 feb 2024</h4>
+        </div>
 
+        <h2 className="mt-5 text-2xl font-semibold">Make a Youtube video</h2>
+        <p className="text-sm mt-2 ">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio,Lorem
+          ipsum dolor sit amet consectetur adipisicing elit. Optio, ?
+        </p>
+      </div>
+
+      <div className="h-full w-[300px] bg-green-400 rounded-xl flex-shrink-0 p-5">
+        <div className=" flex justify-between items-center">
+          <h3 className="bg-red-600 text-sm px-3 py-1 rounded">Hight</h3>
+          <h4 className="text-sm">20 feb 2024</h4>
+        </div>
+
+        <h2 className="mt-5 text-2xl font-semibold">Make a Youtube video</h2>
+        <p className="text-sm mt-2 ">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio,Lorem
+          ipsum dolor sit amet consectetur adipisicing elit. Optio, ?
+        </p>
+      </div>
+
+      <div className="h-full w-[300px] bg-red-400 rounded-xl flex-shrink-0 p-5">
+        <div className=" flex justify-between items-center">
+          <h3 className="bg-red-600 text-sm px-3 py-1 rounded">Hight</h3>
+          <h4 className="text-sm">20 feb 2024</h4>
+        </div>
+
+        <h2 className="mt-5 text-2xl font-semibold">Make a Youtube video</h2>
+        <p className="text-sm mt-2 ">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio,Lorem
+          ipsum dolor sit amet consectetur adipisicing elit. Optio, ?
+        </p>
+      </div>
+
+      <div className="h-full w-[300px] bg-gray-400 rounded-xl flex-shrink-0 p-5">
+        <div className=" flex justify-between items-center">
+          <h3 className="bg-red-600 text-sm px-3 py-1 rounded">Hight</h3>
+          <h4 className="text-sm">20 feb 2024</h4>
+        </div>
+
+        <h2 className="mt-5 text-2xl font-semibold">Make a Youtube video</h2>
+        <p className="text-sm mt-2 ">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio,Lorem
+          ipsum dolor sit amet consectetur adipisicing elit. Optio, ?
+        </p>
+      </div>
+      <div className="h-full w-[300px] bg-pink-400 rounded-xl flex-shrink-0 p-5">
+        <div className=" flex justify-between items-center">
+          <h3 className="bg-red-600 text-sm px-3 py-1 rounded">Hight</h3>
+          <h4 className="text-sm">20 feb 2024</h4>
+        </div>
+
+        <h2 className="mt-5 text-2xl font-semibold">Make a Youtube video</h2>
+        <p className="text-sm mt-2 ">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio,Lorem
+          ipsum dolor sit amet consectetur adipisicing elit. Optio, ?
+        </p>
+      </div>
+      <div className="h-full w-[300px] bg-yellow-400 rounded-xl flex-shrink-0 p-5">
+        <div className=" flex justify-between items-center">
+          <h3 className="bg-red-600 text-sm px-3 py-1 rounded">Hight</h3>
+          <h4 className="text-sm">20 feb 2024</h4>
+        </div>
+
+        <h2 className="mt-5 text-2xl font-semibold">Make a Youtube video</h2>
+        <p className="text-sm mt-2 ">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio,Lorem
+          ipsum dolor sit amet consectetur adipisicing elit. Optio, ?
+        </p>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default TaskList
+export default TaskList;
