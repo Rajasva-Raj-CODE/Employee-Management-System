685b2f5f549f3cee7b6f8dedc7822b173a808b20
Rajasva-Raj-CODE <rajasvarajshrivastava@gmail.com>
2024-10-22 23:38:24 +0530

    changes

---
+++ src/Components/other/AllTask.jsx
@@ -0,0 +1,47 @@
+import React from 'react'
+
+const AllTask = () => {
+  return (
+    <div className='bg-[#1c1c1c] p-5 mt-5 rounded h-48 overflow-auto'>
+
+        <div className=' flex justify-between rounded bg-red-400 py-2 px-4 mb-2'>
+            <h2>Rajasva Raj</h2>
+            <h3>Make a UI Design</h3>
+            <h5>Status</h5>
+        </div>
+
+
+        <div className=' flex justify-between rounded bg-green-400 py-2 px-4 mb-2'>
+            <h2>Rajasva Raj</h2>
+            <h3>Make a UI Design</h3>
+            <h5>Status</h5>
+        </div>
+
+        
+        <div className=' flex justify-between rounded bg-blue-400 py-2 px-4 mb-2'>
+            <h2>Rajasva Raj</h2>
+            <h3>Make a UI Design</h3>
+            <h5>Status</h5>
+        </div>
+
+        <div className=' flex justify-between rounded bg-yellow-400 py-2 px-4 mb-2'>
+            <h2>Rajasva Raj</h2>
+            <h3>Make a UI Design</h3>
+            <h5>Status</h5>
+            </div>
+
+            <div className=' flex justify-between rounded bg-pink-400 py-2 px-4 mb-2'>
+            <h2>Rajasva Raj</h2>
+            <h3>Make a UI Design</h3>
+            <h5>Status</h5>
+            </div>
+            <div className=' flex justify-between rounded bg-indigo-400 py-2 px-4 mb-2'>
+            <h2>Rajasva Raj</h2>
+            <h3>Make a UI Design</h3>
+            <h5>Status</h5>
+            </div>
+    </div>
+  )
+}
+
+export default AllTask
